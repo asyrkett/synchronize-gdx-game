9f072218ce98a0f7e78b6330e99f89c9a6db0550
asyrkett <asyrkett@ggc.edu>
2014-03-23 14:57:42 -0400

    Initial commit

---
+++ synchronize-gdx-game-desktop/src/com/asyrkett/synchronize/Main.java
@@ -0,0 +1,15 @@
+package com.asyrkett.synchronize;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "synchronize-gdx-game";
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new SynchronizeGame(), cfg);
+	}
+}

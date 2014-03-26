a9338b0eb1e0867e47e8895c6687b5efde693b34
asyrkett <asyrkett@ggc.edu>
2014-03-26 19:09:00 -0400

    updated jars

---
+++ synchronize-game/src/com/asyrkett/synchronize/tweens/SpriteAccessor.java
@@ -0,0 +1,21 @@
+package com.asyrkett.synchronize.tweens;
+
+import aurelienribon.tweenengine.TweenAccessor;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class SpriteAccessor implements TweenAccessor<Sprite>
+{
+
+	@Override
+	public int getValues(Sprite target, int tweenType, float[] returnValues) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setValues(Sprite target, int tweenType, float[] newValues) {
+		// TODO Auto-generated method stub
+		
+	}
+}
